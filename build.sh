c0675f17229bd4ff163a99066d2acfc66776d7e7
tapir2342 <github@tapir2342.com>
2022-07-18 21:12:48 +0200

    index + project

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#cp -rv src public
-rsync -avh src/ public
-for f in $(find public -type f -name '*.html'); do
-	sed -i -e '/NAVIGATION/{r partials/navigation.html' -e 'd}' "${f}"
-done
